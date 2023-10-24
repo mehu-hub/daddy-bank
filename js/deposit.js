580507235bba6fc5453d75d678562dd4831adf1c
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-25 02:11:17 +0600

    Deposit Functional

---
+++ js/deposit.js
@@ -0,0 +1,22 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const depositInput = document.getElementById('deposit-input')
+    const newDepositAmountString = depositInput.value;
+
+    const newDepositAmount = parseFloat(newDepositAmountString)
+    const depositTotalElement = document.getElementById('deposit-total');
+
+    const prevDepositTotalstring = depositTotalElement.innerText; 
+    const prevDepositTotal = parseFloat(prevDepositTotalstring)
+
+    const currentDepositTotal = prevDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+    //---------------------------------------------
+    const balanceTotalElement = document.getElementById('balance-total');
+    const prevBalanceTotalString = balanceTotalElement.innerText;
+    const prevBalanceTotal = parseFloat(prevBalanceTotalString);
+
+    const currentBalanaceTotal = prevBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanaceTotal;
+
+    depositInput.value = '';
+})
